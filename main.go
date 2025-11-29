a90ab4c997439a9093157c11876d6f2c6805aa9e
almuiz021 <mrabdulmuizghori@gmail.com>
2025-11-29 06:37:20 +0000

    create a file server for index.html

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	server := http.Server{
+	server := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
+
+	handlerFS := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", handlerFS)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
